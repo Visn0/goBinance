b82cdddf6aa9a03503d5e4b414d9584dc161f7ac
Visn0 <anton_chernysh@outlook.es>
2021-09-05 18:40:45 +0200

    xd

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
+	"net/url"
 
 	"github.com/gorilla/websocket"
 )
@@ -58,10 +58,84 @@ type KlineWebsocketRequest struct {
 	Interval string
 }
 
-func getKline(kwr KlineWebsocketRequest) (chan *KlineEvent, chan struct{}, error) {
-	url := fmt.Sprintf("wss://stream.binance.com:9443/ws/%s@kline_%s", strings.ToLower(kwr.Symbol), string(kwr.Interval))
+type KlineStreamMessage struct {
+	EventType string `json:"e"`
+	EventTime int    `json:"E"`
+	Symbol    string `json:"s"`
+	K         struct {
+		KlineStartTime           int    `json:"t"`
+		KlineCloseTime           int    `json:"T"`
+		Symbol                   string `json:"s"`
+		Interval                 string `json:"i"`
+		FirstTradeID             int    `json:"f"`
+		LastTradeID              int    `json:"L"`
+		OpenPrice                string `json:"o"`
+		ClosePrice               string `json:"c"`
+		HighPrice                string `json:"h"`
+		LowPrice                 string `json:"l"`
+		BaseAssetVolume          string `json:"v"`
+		NumberOfTrades           int    `json:"n"`
+		IsKlineClosed            bool   `json:"x"`
+		QuoteAssetVolume         string `json:"q"`
+		TakerBuyBaseAssetVolume  string `json:"V"`
+		TakerBuyQuoteAssetVolume string `json:"Q"`
+		Ignore                   string `json:"B"`
+	} `json:"k"`
+}
+
+func (k KlineStreamMessage) String() string {
+	js, _ := json.MarshalIndent(k, "", "\t")
+	s := string(js)
+	return s
+}
+
+func getKline(kwr KlineWebsocketRequest) {
+	// url := fmt.Sprintf("wss://stream.binance.com:9443/ws/%s@kline_%s", strings.ToLower(kwr.Symbol), string(kwr.Interval))
+	// request := fmt.Sprintf("{\"method\":\"SUBSCRIBE\",\"params\":[\"btcusdt@kline_1m\"],\"id\":1}\"")
+	host := "stream.binance.com"
+	port := "9443"
+	topic := "btcusdt@kline_1m"
+	m := make(map[string]interface{})
+	m["method"] = "SUBSCRIBE"
+	m["params"] = topic
+	m["id"] = 1
+
+	u := url.URL{Scheme: "wss", Host: host + ":" + port, Path: "ws/" + topic}
+	log.Println(u.String())
+	log.Println(m)
+
+	c, hr, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println(hr.Status)
+
+	// err = c.WriteJSON(m)
+	if err != nil {
+		log.Println(err)
+	}
+	go func() {
+		defer c.Close()
+		log.Println()
+		for {
+			var msg KlineStreamMessage
+			err := c.ReadJSON(&msg)
+			if err != nil {
+				log.Fatal("[READ ERROR]: \n", err)
+			}
+			log.Println("[MESSAGE]: ", msg)
+
+		}
+	}()
 }
 
 func main() {
 	fmt.Println("Hello world")
+	kwr := KlineWebsocketRequest{Symbol: "btcusdt", Interval: "1m"}
+	getKline(kwr)
+
+	// fmt.Scanf()
+	for {
+	}
 }
