503225100a1f679610cb67ccd92e8a0199d3ea04
Visn0 <anton_chernysh@outlook.es>
2021-09-06 10:22:14 +0200

    refactoriced to different packages

---
+++ stream/websocket.go
@@ -0,0 +1,42 @@
+package stream
+
+import (
+	"log"
+
+	btypes "github.com/Visn0/goBinance/binanceTypes"
+	"github.com/gorilla/websocket"
+)
+
+type WebSocket struct {
+	Conn *websocket.Conn
+	// Out  chan []byte
+	// In   chan []byte
+	// Events map[string]EventHandler
+}
+
+func (ws *WebSocket) Connect(url string) {
+	log.Printf("Connecting to %v", url)
+
+	wsd, _, err := websocket.DefaultDialer.Dial(url, nil)
+	if err != nil {
+		log.Fatal("[DIAL ERROR]: \n", err)
+	}
+	ws.Conn = wsd
+}
+
+func (ws *WebSocket) Close() {
+	ws.Conn.Close()
+}
+
+func (ws *WebSocket) Subscribe(request btypes.SubscribeMessage) {
+	err := ws.Conn.WriteJSON(request)
+	if err != nil {
+		log.Fatal("[SUBSCRIBE ERROR]: \n", err)
+	}
+
+	_, msg, err := ws.Conn.ReadMessage()
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println("[SUSCRIPTION RESPONSE", string(msg))
+}
